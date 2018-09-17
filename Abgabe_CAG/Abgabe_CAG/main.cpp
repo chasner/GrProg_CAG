27fb914f55ffe34e1d9b4209782b4fc9367da1d3
cag <cgussner@live.de>
2018-09-17 16:07:06 +0200

    Little Fixes

--- Abgabe_CAG/Abgabe_CAG/main.cpp
+++ Abgabe_CAG/Abgabe_CAG/main.cpp
@@ -11,17 +11,22 @@ bool hasMatchfieldCreated = false;
 // This Vector consists off every 
 std::vector<sf::RectangleShape> matchshapes(49);
 
-// This Function builds a 7 x 7 Matchfield
-void CreateMatchfield(int _ShapesToRender)
+// This Function builds a 7 x 7 Matchfield 
+void CreateMatchfield()
 {
 	if (hasMatchfieldCreated == false)
 	{
+
 		for (int i = 1; i < 8; i++)
 		{
 			for (int j = 1; j < 8; j++)
 			{
 				std::cout << "Creating Square[" << i << "][" << j << "]\n";
 				// Create Square[i][j] 
+				// sf::RectangleShape Square1(25.00*i,25.00*j);
+				//Square1.setFillColor(sf::Color::White);
+
+
 			}
 		}
 		hasMatchfieldCreated = true;
@@ -30,17 +35,38 @@ void CreateMatchfield(int _ShapesToRender)
 }
 
 
+
+
 int main()
 {
 	std::cout << "Welcome to the Game! \n";
 
 	// Creates the window for the game. The player should only be able to close and move the window. Titlebar / Resize
-	sf::RenderWindow gamewindow(sf::VideoMode(800, 800),"Abgabe_CAG", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow gamewindow(sf::VideoMode(750, 750),"Abgabe_CAG", sf::Style::Close | sf::Style::Titlebar);
 
+	CreateMatchfield();
 
+	// Refactor this!
+	sf::RectangleShape square1(sf::Vector2f(50.00f, 50.00f));
+	square1.setFillColor(sf::Color::White);
 
-	CreateMatchfield(2);
+	sf::RectangleShape square2(sf::Vector2f(50.00f, 50.00f));
+	square2.setFillColor(sf::Color::White);
 
+	sf::RectangleShape square3(sf::Vector2f(50.00f, 50.00f));
+	square3.setFillColor(sf::Color::White);
+
+	sf::RectangleShape square4(sf::Vector2f(50.00f, 50.00f));
+	square3.setFillColor(sf::Color::White);
+
+	sf::RectangleShape square5(sf::Vector2f(50.00f, 50.00f));
+	square3.setFillColor(sf::Color::White);
+
+	sf::RectangleShape square6(sf::Vector2f(50.00f, 50.00f));
+	square3.setFillColor(sf::Color::White);
+
+	sf::RectangleShape square7(sf::Vector2f(50.00f, 50.00f));
+	square3.setFillColor(sf::Color::White);
 	
 	// Gameloop
 	while (gamewindow.isOpen())
@@ -73,14 +99,35 @@ int main()
 
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			//sf::Vector2i mousePos = sf::Mouse::getPosition(gamewindow);
+			sf::Vector2i mousePos = sf::Mouse::getPosition(gamewindow);
+			sf::Vector2f square1position = square1.getPosition();
+
+			if (mousePos.x > 50 && mousePos.x < 100)
+			{
+				square1.setFillColor(sf::Color::Green);
+				std::cout << "Player clicked: " << "square1" << "\n";
+			}
+
 			//defaultDot2.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
 		}
 
 
 		gamewindow.clear(sf::Color::Black);
 
-
+		square1.setPosition(50, 50);
+		gamewindow.draw(square1);
+		square2.setPosition(150, 50);
+		gamewindow.draw(square2);
+		square3.setPosition(250, 50);
+		gamewindow.draw(square3);
+		square4.setPosition(350, 50);
+		gamewindow.draw(square4);
+		square5.setPosition(450, 50);
+		gamewindow.draw(square5);
+		square6.setPosition(550, 50);
+		gamewindow.draw(square6);
+		square7.setPosition(650, 50);
+		gamewindow.draw(square7);
 
 		
 		
