54cab4c520d77984875acd5a04cb53cbb45221cc
Christoph Gussner <chgussner@mediadesign.de>
2018-10-02 09:56:53 +0200

    comment out lines

--- Abgabe_CAG/Abgabe_CAG/Tile/Tile.cpp
+++ Abgabe_CAG/Abgabe_CAG/Tile/Tile.cpp
@@ -1,10 +1,10 @@
-
 #include "Tile.h"
 
-
+/*
 Tile::Tile() : myType(ETileType::DEFAULT), myGridPosition(sf::Vector2i(-1, -1))
 {
 	setFillColor(sf::Color::Green);
 	setOutlineThickness(-5.0f);
 	setOutlineColor(sf::Color::Red);
 }
+*/
